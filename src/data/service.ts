da545390d6ed187c88537476d10d6b13836931f4
badusha061 <badubadusha061@gmail.com>
2025-05-12 01:01:01 +0530

    Added

--- src/data/service.ts
+++ src/data/service.ts
@@ -8,42 +8,42 @@ const Services : ServieType[] = [
     {
         "id" : 1,
         "name" : "Radiation Protection & X-Ray",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746991801/al%20rawda/x_rcnfhl.jpg"
     },
     {
         "id" : 2,
         "name" : "CT Scan, Cat lab Opg",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083784/al%20rawda/oyl1uua4dhlgxyrk6itu.jpg"
     },
     {
         "id" : 3,
         "name" : "Pre Installation Work",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746991207/al%20rawda/pre_install_vyu1wq.webp"
     },
     {
         "id" : 4,
         "name" : "Lead Shiedling Work",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746977386/al%20rawda/lead_sheid_igpmgh.jpg"
     },
     {
         "id" : 5,
         "name" : "Lead Shielding Door",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746991686/al%20rawda/x-ray_vjgzvd.jpg"
     },
     {
         "id" : 6,
         "name" : "Lead Glass",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746990733/al%20rawda/lead_glass_u89bqa.jpg"
     },
     {
         "id" : 7,
         "name" : "X-Ray Warning Lights",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083336/al%20rawda/fd1bmfrdmb1krffndldd.jpg"
     },
     {
         "id" : 8,
         "name" : "Floor Trunking",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746977836/al%20rawda/floor_trucking_nsyqow.jpg"
     },
     {
         "id" : 9,
@@ -53,17 +53,17 @@ const Services : ServieType[] = [
     {
         "id" : 10,
         "name" : "X-Ray Control Panel",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746991545/al%20rawda/x-ray-control_lueddv.jpg"
     },
     {
         "id" : 11,
         "name" : "Koriyan Top",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746978007/al%20rawda/koriyan_i7yqyo.jpg"
     },
     {
         "id" : 12,
         "name" : "Glass Partition",
-        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1745083529/al%20rawda/nnp2ph1482mkjjkh85zl.jpg"
+        "image" : "https://res.cloudinary.com/dzvxebtnl/image/upload/v1746990885/al%20rawda/glass_part_ycsaz9.jpg"
     }
 ]
 
