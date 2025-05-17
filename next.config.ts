8ba7f1cb89ca26c64de7924fc5d04d889ad3843d
badusha061 <badubadusha061@gmail.com>
2025-05-17 08:49:55 +0530

    change the config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images:{
-    domains:["res.cloudinary.com","images.unsplash.com"]
+    domains:["res.cloudinary.com"]
   }
 };
 
